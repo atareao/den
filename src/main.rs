bea8bd956f10bda9cac26143b8a15e1f5bd1d85b
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-02 04:34:42 +0100

    settings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
 mod mattermost;
+mod telegram;
 
 use futures::StreamExt;
-use shiplift::Docker;
+use shiplift::{Docker, rep::Event};
 use tokio::fs;
-use yaml_rust::YamlLoader;
+use yaml_rust::{YamlLoader, Yaml};
+use log::{error, info};
+use env_logger::Builder;
+use std::process;
 
 #[tokio::main]
 async fn main() {
-    let content = fs::read_to_string("config.yml")
-        .await
-        .expect("config.yml not found");
-    let configuration = YamlLoader::load_from_str(&content)
-        .expect("Something happened");
+    let content = match fs::read_to_string("config.yml")
+        .await {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error with config file `config.yml`: {}",
+                    e.to_string());
+                process::exit(0);
+            }
+        };
+    let data = match YamlLoader::load_from_str(&content){
+        Ok(value) => value[0]
+            .clone()
+            .as_hash()
+            .expect("someting happened with `config.yml`")
+            .clone(),
+        Err(e) => {
+            println!("Cant read the config file `config.yml`: {}",
+                e.to_string());
+            process::exit(0);
+        }
+    };
+    let settings = data.get(&Yaml::from_str("settings"))
+        .expect("Can't read settings")
+        .as_hash()
+        .unwrap();
+
+
+    let log_level = settings.get(&Yaml::from_str("logging"))
+        .unwrap()
+        .as_str()
+        .unwrap_or("info");
+
+    Builder::new()
+        .filter_level(match log_level{
+            "trace" => log::LevelFilter::Trace,
+            "debug" => log::LevelFilter::Debug,
+            "info" => log::LevelFilter::Info,
+            "warn" => log::LevelFilter::Warn,
+            "error" => log::LevelFilter::Error,
+            _ => log::LevelFilter::Off,
+        })
+        .parse_default_env()
+        .init();
+
+    info!("Configuration loaded");
+
     let docker = Docker::new();
-    println!("listening for events");
+    info!("listening for events");
 
     while let Some(event_result) = docker.events(&Default::default()).next().await {
         match event_result {
@@ -21,4 +66,8 @@ async fn main() {
             Err(e) => eprintln!("Error: {}", e),
         }
     }
+}
+
+fn process(event: &Event, configuration: &Vec<Yaml>){
+
 }
