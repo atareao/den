9594f3335167a29d18be18087981e99a2157d35d
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-26 21:22:20 +0200

    :sparkles: support matrix

--- src/publisher.rs
+++ src/publisher.rs
@@ -6,6 +6,8 @@ use std::str::FromStr;
 use std::collections::HashMap;
 use anyhow::{Error, anyhow};
 use log::{error, debug, info};
+use urlencoding::encode;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Publisher{
@@ -17,17 +19,20 @@ pub struct Publisher{
 const MATTERMOST: &str = "mattermost";
 const TELEGRAM: &str = "telegram";
 const ZINC: &str = "zinc";
+const MATRIX: &str = "matrix";
 
 
 impl Publisher{
 
     pub async fn post_message(&self, message: &str) -> Result<Response, Error>{
-        if self.service.to_lowercase() == MATTERMOST{
+        if self.service.to_lowercase() == MATTERMOST {
             return Ok(self.post_with_mattermost(message).await?);
-        }else if self.service.to_lowercase() == TELEGRAM{
+        }else if self.service.to_lowercase() == TELEGRAM {
             return Ok(self.post_with_telegram(message).await?);
-        }else if self.service.to_lowercase() == ZINC{
+        }else if self.service.to_lowercase() == ZINC {
             return Ok(self.post_with_zinc(message).await?);
+        }else if self.service.to_lowercase() == MATRIX {
+            return Ok(self.post_with_matrix(message).await?);
         }else{
             error!("Publisher not defined: {}", self.service);
             Err(anyhow!("Publisher not defined: {}", self.service))
@@ -71,6 +76,24 @@ impl Publisher{
         Self::post(&url, header_map, &body).await
     }
 
+    async fn post_with_matrix(&self, message: &str) -> Result<Response, reqwest::Error>{
+        debug!("Post with matrix: {}", message);
+        let token = self.config.get("token").unwrap();
+        let room = encode(self.config.get("room").unwrap());
+        let base_url = self.config.get("url").unwrap();
+        let uuid = Uuid::new_v4().to_string();
+        let url = format!("https://{}/_matrix/client/v3/rooms/{}:{}/send/m.room.message/{}", base_url, room, base_url, uuid);
+        let body = json!({
+                "body": message,
+                "msgtype": "m.text",
+        });
+        let mut header_map = HeaderMap::new();
+        header_map.insert(HeaderName::from_str("Content-type").unwrap(),
+                          HeaderValue::from_str("application/json").unwrap());
+        header_map.append(HeaderName::from_str("Authorization").unwrap(),
+                          HeaderValue::from_str(&format!("Bearer {}", token)).unwrap());
+        Self::put(&url, header_map, &body).await
+    }
 
     async fn post_with_mattermost(&self, message: &str) -> Result<Response, reqwest::Error>{
         debug!("Post with mattermost: {}", message);
@@ -96,6 +119,14 @@ impl Publisher{
             .unwrap();
         let content = serde_json::to_string(body).unwrap();
         client.post(url).body(content).send().await
+    }
+    async fn put(url: &str, header_map: HeaderMap, body: &Value) -> Result<Response, reqwest::Error>{
+        let client = Client::builder()
+            .default_headers(header_map)
+            .build()
+            .unwrap();
+        let content = serde_json::to_string(body).unwrap();
+        client.put(url).body(content).send().await
     }
 }
 
